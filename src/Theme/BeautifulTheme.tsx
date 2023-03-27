cfe38e7a2ac10c032fa371cce753fb3eff2fdd60
maxymlyskov <raypember02@gmail.com>
2023-03-27 16:42:22 +0300

    Added beautiful theme

--- src/Theme/BeautifulTheme.tsx
+++ src/Theme/BeautifulTheme.tsx
@@ -1,5 +1,31 @@
 import { createTheme } from "@mui/material/styles";
 
-const BeautifulTheme = createTheme({});
+declare module '@mui/material/styles/createPalette'{
+    interface Palette {
+        grid : {
+            main:string,
+            dark: string
+        }
+    }
+    interface PaletteOptions {
+        grid? : {
+            main:string,
+            dark: string
+        }
+    }
+}
+
+const BeautifulTheme = createTheme({
+    palette :{
+        primary: {
+            main: "#188842c",
+            dark: '#darkgreen'
+        },
+        grid: {
+            main: "#182842c",
+            dark: '#darkblue'
+        }
+    }
+});
 
 export { BeautifulTheme };
