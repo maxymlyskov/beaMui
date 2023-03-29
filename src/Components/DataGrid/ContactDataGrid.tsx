cd14d94bd30bb30d04f999b702c604bb19e9ae8d
maxymlyskov <raypember02@gmail.com>
2023-03-29 16:44:44 +0300

    Added collapse and datagrid toolbar.

--- src/Components/DataGrid/ContactDataGrid.tsx
+++ src/Components/DataGrid/ContactDataGrid.tsx
@@ -1,7 +1,8 @@
-import { useCallback } from "react";
-import { DataGrid, GridRenderCellParams } from "@mui/x-data-grid";
+import { useCallback, useState } from "react";
+import { DataGrid, GridRenderCellParams, GridToolbar, GridToolbarContainer, GridToolbarExport } from "@mui/x-data-grid";
 import { useTheme, Theme } from '@mui/material/styles';
 import { contactData } from "../../Data/ContactData";
+import { Button, Collapse } from "@mui/material";
 
 const getColumns = (theme: Theme) => {
   return [
@@ -71,16 +72,28 @@ const getColumns = (theme: Theme) => {
 
 export default function ContactDataGrid() {
   const theme = useTheme();
+  const [open, setOpen] = useState(true)
   const rows = () => [...contactData];
   return (
     <div style={{ height: 500 }}>
-      <DataGrid
-        rows={rows()}
-        columns={getColumns(theme)}
-        pageSize={5}
-        headerHeight={60}
-        rowHeight={120}
-      />
+      <Button onClick={()=>setOpen(!open)}>Collapse</Button>
+      {/* <Collapse in={open}> */}
+        <DataGrid
+          rows={rows()}
+          columns={getColumns(theme)}
+          pageSize={5}
+          headerHeight={60}
+          rowHeight={120}
+          components={
+            {Toolbar: () => ( <GridToolbarContainer sx={{justifyContent:'flex-end', "& button ": {border: "none"}, "& .MuiBox-root":{display:"none"}}}>
+              <GridToolbarExport/>
+            </GridToolbarContainer>)}
+          }
+          initialState={{
+            sorting: { sortModel: [{field: "name", sort: 'asc' }]}
+          }}
+          />
+        {/* </Collapse> */}
     </div>
   )
 }
